251afbfd883757873468dcac7f1cbc2fa05a59e9
Kapilrajput089 <kapil335286@gmail.com>
2024-06-03 21:34:25 +0530

    First commit of C Programming

---
+++ _1_C_Programming/Add.c
@@ -0,0 +1,15 @@
+// write a program to Add N numbers wiht kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+    int a,b,c;
+    printf("Enter First number here : = ");
+    scanf("%d",&a);
+    printf("Enter Second number here : = ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Addition of A and B is = %d",c);
+    getch();
+    return 0;
+}
