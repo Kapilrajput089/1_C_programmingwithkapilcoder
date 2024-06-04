9388b256c0f75bd2af56b84194b2c4246fa035d6
Kapilrajput089 <kapil335286@gmail.com>
2024-06-04 10:20:25 +0530

    My first commit of C Programming

---
+++ _1_C_Programming/ArithmeticOperators.c
@@ -0,0 +1,17 @@
+// write a program to use Arithmeri operator in C with kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+    printf("Arithmeic Operator in C programming\n");
+    int a=70,b=40;
+    printf("Addition of A + B = %d\n ",(a+b));
+    printf("Subtraction of A - B =  %d\n",(a-b));
+    printf("Multiplication of A * B = %d\n",(a*b));
+    printf("Division of  A / B = %d\n",(a/b));
+    printf("Modulus of A %% B = %d",(a% b));
+    // These all are  Arithetic operators in C programming.
+    getch();
+    return 0;
+
+}
