dcc8c373c7e1dbcfe200f4ba0fbd25ab234c4270
Kapilrajput089 <kapil335286@gmail.com>
2024-06-05 13:51:35 +0530

    My first commit of C programming.

---
+++ _1_C_Programming/AssignmentOprators.c
@@ -0,0 +1,23 @@
+// write a program to use all Assignment Operators in C with kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+    printf("Assignment Operators in C programming\n");
+    int a=5,b=5;
+    // Assignment Operators are :-(=, +=, -=, *=, /=)
+    printf("1. A = : %d\n",a);
+    b+=a;
+    printf("2. B += : %d\n",b);
+    b-=10;
+    printf("3. B -= : %d\n",b);
+    a*=b;
+    printf("4. A *= : %d\n",a);
+    a/=8;
+    printf("5. A /= : %d\n",a);
+    // These all Assinment Operators in C programming.
+    getch();
+    return 0;
+
+
+}
