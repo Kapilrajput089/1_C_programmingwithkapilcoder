a9a7bfcb83dfbedfc04b4c8e308c9e2ebfd48ef0
Kapilrajput089 <kapil335286@gmail.com>
2024-06-05 18:04:12 +0530

    My first commit C _1_C_Programming

---
+++ _1_C_Programming/BitwiseOperators.c
@@ -0,0 +1,13 @@
+//  write a program to use all Bitwise Operatorsin C programming with kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main(){
+    printf("Bitwise Operators in C programming\n");
+    int a=5,b=6;
+    // Bitwise operators are :- (&, |, ^).
+    printf("1. A&B is %d\n",(a& b));
+    printf("2. A|B is %d\n",(a|b));
+    printf("3. A^B is %d\n",(a^b));
+    getch();
+    return 0;
+}
