6074540e29156049d5eb60e3b0ad70451eb5783a
Kapilrajput089 <kapil335286@gmail.com>
2024-06-06 19:59:42 +0530

    My first commit of C programming with kapilcoder.

---
+++ _1_C_Programming/ConditionalOperators.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+      printf("Conditional Operator in C programming\n");
+    // conditional Operator (?:).
+    int x=8,y=10;
+    x<y?printf("1. X : =  %d\n",x):printf("2. Y : = %d\n",y);
+    getch();
+    return 0;
+
+}
--- _1_C_Programming/tempCodeRunnerFile.c
+++ _1_C_Programming/tempCodeRunnerFile.c
@@ -1,13 +1,1 @@
-//  write a program 
-#include"stdio.h"
-#include"conio.h"
-int main(){
-    int a=5,b=6;
-    printf("1. A&B is %d\n",(a& b));
-    printf("2. A|B is %d\n",(a|b));
-    printf("3. A^B is %d\n",(a^b));
-    //printf("4. A>>B is %d\n",(a>>B));
-    //printf("5. A<<B is %d\n",(a<<b));
-    getch();
-    return 0;
-}
+  printf("Logical Operators in C programming\n");
