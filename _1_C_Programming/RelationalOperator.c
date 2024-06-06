0b9af3958d0385df51a89ba441af95223e55f14d
Kapilrajput089 <kapil335286@gmail.com>
2024-06-06 19:08:57 +0530

    My first commit of C programming.

---
+++ _1_C_Programming/RelationalOperator.c
@@ -0,0 +1,16 @@
+#include"stdio.h"
+#include"conio.h"
+int main(){
+
+       printf("Relational Operators in C programming\n");
+       // Relational Operators are :-(<, >, <=, >=, ==, !=).
+       int a=5,b=6;
+       printf("1. Is A > B : %d\n", (a> b));
+       printf("2. Is A < B : %d\n", (a< b));
+       printf("3. Is A <= B : %d\n", (a<= b));
+       printf("3. Is A >= B : %d\n", (a>= b));
+       printf("3. Is A == B : %d\n", (a== b));
+       printf("3. Is A != B : %d\n", (a!= b));
+       getch();
+       return 0;
+}
