4c3b88e277d9c774c8fdb67f91003e2814eafe98
Kapilrajput089 <kapil335286@gmail.com>
2024-06-06 19:33:20 +0530

    My first commit of C programming.

---
+++ _1_C_Programming/LogicalOperators.c
@@ -0,0 +1,16 @@
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+    
+       printf("Logical Operators in C programming\n");
+       // Logical Operators are :-(!, &&, ||).
+       int a=2,b=8;
+          printf("1. Is A ! B : %d\n",!(a&& b));
+          printf("1. Is A && B : %d\n",!(a|| b));
+          printf("1. Is A || B : %d\n",(a|| b));
+          printf("1. Is A && B : %d\n",(a&& b));
+          getch();
+          return 0;
+
+}
