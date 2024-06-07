c795404eea6ba92a83e12ce84053287e70580ffd
Kapilrajput089 <kapil335286@gmail.com>
2024-06-07 14:45:19 +0530

    My first commit of C programming with kapilcoder.

--- _1_C_Programming/ArithmeticOperators.c
+++ _1_C_Programming/ArithmeticOperators.c
@@ -1,9 +1,10 @@
-// write a program to use Arithmeri operator in C with kapilcoder.
+// write a program to use Arithmetic operator in C with kapilcoder.
 #include"stdio.h"
 #include"conio.h"
 int main()
 {
     printf("Arithmeic Operator in C programming\n");
+     // Arithmetic operators are :- (+, -, *, /, %).
     int a=70,b=40;
     printf("Addition of A + B = %d\n ",(a+b));
     printf("Subtraction of A - B =  %d\n",(a-b));
