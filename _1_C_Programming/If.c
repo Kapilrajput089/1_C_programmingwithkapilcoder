918937fc44a98d6d0a835e3c800eb50608f00fb6
Kapilrajput089 <kapil335286@gmail.com>
2024-06-07 17:01:39 +0530

    My first commit of C programming with kapilcoder

---
+++ _1_C_Programming/If.c
@@ -0,0 +1,20 @@
+//Write a program to If statement in C with kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+     printf("if else statement in C programming\n");
+    int x;
+    printf("Please Enter Number : = ");
+    scanf("%d",&x);
+    if(x>0)
+    {
+        printf("Number is positive ");
+    }
+    if(x<0)  {
+    printf("Number is non positive ");
+    }
+    //That's all about if statement in programming .
+    getch();
+    return 0;
+}
