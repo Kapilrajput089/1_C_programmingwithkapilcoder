9acaab37598e9a6855f84889f8aa0c6def96edd0
Kapilrajput089 <kapil335286@gmail.com>
2024-06-09 18:01:56 +0530

    My first commit of C programming With kapilcoder

---
+++ _1_C_Programming/WhileLoop.c
@@ -0,0 +1,14 @@
+//Write a program to Use While loop in C programming with kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+   printf("While loop in C programming\n");
+    int i=1;// initialization
+    while(i<=10){// condition
+        printf("Value of i : = %d\n",i);
+        i++; // fow
+    }
+    getch();
+    return 0;
+}
