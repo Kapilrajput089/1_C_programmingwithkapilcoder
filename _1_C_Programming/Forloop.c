e713a2897eb13a4a4b914800c4514c8efe63124a
Kapilrajput089 <kapil335286@gmail.com>
2024-06-09 17:54:50 +0530

    My first commit of C programming with kapilcoder

---
+++ _1_C_Programming/Forloop.c
@@ -0,0 +1,17 @@
+//Write a program to Use For loop in C programming with kapilcoder.
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+    int i;
+    /* There are three types of C programming.
+    1.=> For loop.
+    2.=> While loop.
+    3.=> Do while loop.
+    */
+   for(int i=1;i<=10;i++)
+   printf("Value of i : = %d\n",i);
+   getch();
+   return 0;
+
+}
