13d0e1f492ad5cb8420ce9c0316f5884f4eb954f
Kapilrajput089 <kapil335286@gmail.com>
2024-06-12 22:07:38 +0530

    My commit of C programming with kapilcoder

---
+++ _1_C_Programming/Nnaturlnumbers.c
@@ -0,0 +1,13 @@
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+    int n;
+    printf("Enter a number : = ");
+    scanf("%d",&n);
+      printf("N Naturl Numbers are :- \n");
+  for(int i=1;i<=n;i++)
+  printf("%d\n",i);
+getch();
+return 0;
+}
