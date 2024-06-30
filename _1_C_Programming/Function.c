85be13a64f7b31fdd1ff2f6a737b112c41b3686f
Kapilrajput089 <kapil335286@gmail.com>
2024-06-30 12:03:00 +0530

    My second commit of c programming with kapilcoder

--- _1_C_Programming/Function.c
+++ _1_C_Programming/Function.c
@@ -1,19 +1,48 @@
 // write a program to add two number using function  take nothing return nothing .
 #include"stdio.h"
 #include"conio.h"
-void add();
+void fun1(void);// function declaration or prototype
+int fun2(void);// function declaration or prototype
+void fun3(int );// function declaration or prototype
+int fun4(int n );// function declaration or prototype
 int main(){
-  
-  add();
-  getch();
-  return 0;
+    printf("Function in C programming\n ");
+/*There are two type of function in C programming .
+  1.=> pre-defined function .
+  2.=> User defined function.
+  ->pre-defined functions are already declared into the header files.
+  -> User defined functions are created by programmers.
+  and a function have three things.
+  which is (i)=> functuin Declaration.
+           (ii)=> function call.
+           (iii)=> function Definition .
+*/
+
+
+fun1();
+int n= fun2();
+printf(" I am fun2 : %d\n ",n);
+fun3(3);
+fun4(3);
+return 0;
 
 
 }
-void add(){
-  int a,b,res;
-    printf("Enter two Numbers : = ");
-    scanf("%d%d",&a,&b);
-    res=a+b;
-    printf("Addition of two Numbers : %d",res);
+void fun1(void){
+         printf("Function to take nothing and return nothing.\n");
 }
+int fun2(void){
+            printf("Function to take  nothing and return somthing.\n");
+
+}
+
+
+
+void fun3(int n)
+{
+    printf("Function to take somthing and Return nothing %d\n",n);
+}
+int fun4(int n){
+     printf("Function to take  somthing and return somthing.%d\n",n);
+}
+
--- _1_C_Programming/FunctionTwonumberadd.c
+++ _1_C_Programming/FunctionTwonumberadd.c
@@ -1,17 +1,17 @@
 // write  program to add two using function take nothing return somthing .
 #include"stdio.h"
 #include"conio.h"
-int add();
+int add(); //function declaration .
 int main(){
 
     int res;
-    res=add();
+    res=add(); // function call .
     printf("Addition of two numbers %d",res);
     getch();
     return 0;
 
 }
-int add()
+int add()/// function definetion 
 {
     int a,b,res;
     printf("Enter two  Numbers : = ");
