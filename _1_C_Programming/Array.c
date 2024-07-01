be7bc6ca7ca46fa8ea7ded301bc0fecd5c154c34
Kapilrajput089 <kapil335286@gmail.com>
2024-07-01 15:53:12 +0530

    My first commit of C programming with kapilcoder

--- _1_C_Programming/Array.c
+++ _1_C_Programming/Array.c
@@ -8,8 +8,12 @@ printf("Array in c programming \n\n");
 /* 
   * Array is a linar collection of similar elements.
   * it is used to stored at contigious memory location.
+  * 1.=> A one-dimensional array is like a list.
+  * 2.=> A two-dimensional array is like a table.
 
-
+  * There are two ways more about Array in  c prgramming.   
+  1.=> int arr[5];  // this is Called array in c programming.
+  2.=> int arr[5]={10,30,40,50,60,70};// this is called array declaration with initialization.
 */
 int arr[5];
     arr[0]=10;
@@ -23,4 +27,14 @@ int arr[5];
     printf("The value arr at index %d is : %d\n",i,arr[i]);
     //this is called one-dimensional array.
     //and also we can get stored array values by taking from the use using loops.
+    int arr1[2][4]={{3,5,7,4},{1,6,2,8}};
+    printf("it's two dimensional Array.\n ");
+    for(int i=0;i<=2;i++){
+      for(int j=0;j<=3;i++){
+        printf("The value of arr1 at index %d,%d is : %d\n",i,j,arr1[i][j]);
+        getch();
+        return 0;
+
+      }
+    }
 }
